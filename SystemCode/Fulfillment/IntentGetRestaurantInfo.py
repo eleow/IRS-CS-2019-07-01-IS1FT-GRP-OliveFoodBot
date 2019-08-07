b94a8f7dfe552b6fc1e8fef1f78c33210242f978
eleow <eleow@users.noreply.github.com>
2019-08-07 11:32:29 +0800

    Update IntentGetRestaurantInfo.py
    Added code to use yelp api for retrieving of restaurant info.
    TODO: check if daily limit is exceeded, if so, switch to web scrap

--- SystemCode/Fulfillment/IntentGetRestaurantInfo.py
+++ SystemCode/Fulfillment/IntentGetRestaurantInfo.py
@@ -3,13 +3,98 @@ import requests
 import json
 
 from lxml import html
+import re
+import urllib.parse as urllib
+from urllib.error import HTTPError
+from urllib.parse import quote
+from urllib.parse import urlencode
+import pprint
 
-# import builtins as exceptions
-# from time import sleep
-import re,urllib.parse as urllib
-# import argparse
+# Using Yelp API v3
+YELP_API_KEY = "rG5TOrDyCq0G-lIelg9XzKfBcSNrc2F7zsa3C99Nray3q_-Wz8YU1Jdi1rAu7-gSQwdKCuZA0b9GXCp5xMImW9_dxQo_9ib4OAJ-PXRyqPGakfQD8WHL8BX7uDNJXXYx"
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'
 
-def parse(url):
+# Defaults
+DEFAULT_LOCATION = "Singapore"
+SEARCH_LIMIT = 5
+DEBUG_MODE = False
+
+def yelp_request(host, path, api_key, url_params={}):
+    """Given API_KEY, send a GET request to the Yelp REST API.
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        API_KEY (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {'Authorization': 'Bearer %s' % api_key}
+    if (DEBUG_MODE): print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+    if (response.headers.get("RateLimit-Remaining") == '0'):
+        print("Oops.. We have exceeded Yelp Fusion API daily access limit... Please try again tomorrow!")
+    return response.json()
+
+
+def yelp_search(api_key, term, location):
+    """Query Yelp Search API by a search term and location.
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+    Returns:
+        dict: The JSON response from the request.
+    """
+    url_params = {'term': term.replace(
+        ' ', '+'), 'location': location.replace(' ', '+'), 'limit': SEARCH_LIMIT}
+    return yelp_request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
+
+
+def yelp_business(api_key, business_id):
+    """Query Yelp Business API by a business ID.
+    Args:
+        business_id (str): The ID of the business to query.
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+    return yelp_request(API_HOST, business_path, api_key)
+
+
+def yelp_query_api(term, location):
+    """Queries the API by the input values from the user.
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.`
+    """
+    # Check if this business exists. If exists, we get its ID
+    response = yelp_search(YELP_API_KEY, term, location)
+    businesses = response.get('businesses')
+
+    if not businesses:
+        print(u'No businesses for {0} in {1} found.'.format(term, location))
+        return None
+
+    business_id = businesses[0]['id']
+
+    # If exists, then we get its details
+    print(u'{0} businesses found, querying business info ' \
+        'for the top result "{1}" ...'.format(
+            len(businesses), business_id))
+    response = yelp_business(YELP_API_KEY, business_id)
+
+    print(u'Result for business "{0}" found:'.format(business_id))
+    if (DEBUG_MODE): pprint.pprint(response, indent=2)
+    return response
+
+
+def scrape(url):
     # url = "https://www.yelp.com/biz/frances-san-francisco"
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
     response = requests.get(url, headers=headers, verify=False).text
@@ -107,38 +192,73 @@ def parse(url):
 
 def processRestaurantInfoIntent(req):
     RestaurantName = req["queryResult"]["parameters"]["RestaurantName"]
-    Rest_Name = RestaurantName
+
+    Location = req["queryResult"]["parameters"].get("Location")
+    if (Location == None): Location = DEFAULT_LOCATION
+
+    address = Location if isinstance(Location, str) else Location.get("street-address")
+    if (address == "" or address == None): address = Location.get("city")
+    if (address == "" or address == None): address = Location.get("country")
+    if (address == "" or address == None): address = Location.get("business-name")
+    if (address == "" or address == None): address = DEFAULT_LOCATION
 
     if RestaurantName != "":
         RestaurantName = RestaurantName.replace(" ","-")
         RestaurantName = RestaurantName + "-Singapore"
-        infoCount, website, RestName, address, phone, category, price_range, officeHours, take_out, delivery, reservation = getRestaurantInfoIntentHandler(RestaurantName)
+        r = getRestaurantInfoIntentHandler(RestaurantName, address)
+        # infoCount, website, RestName, address, phone, category, price_range, officeHours, take_out, delivery, reservation = getRestaurantInfoIntentHandlerByScraping(RestaurantName, Location)
     else:
         RichMessages = "Restaurant Name is null ..."
 
+
+    subTitle = "Address:\t %s. \nPhone:\t %s. \nPrice Range:\t %s. \nOpening Hours:\t %s. \nCategory:\t %s." % (r["address"], r["phone"], r["price_range"], r["hours"], r["category"])
     defaultPayload = "DEFAULT PAYLOAD"
     defaultRichMessage = [
             {
                 "card": {
-                    "title": RestName, "subtitle": address,
+                    "title": r["name"], "subtitle": subTitle,
                     "imageUri": "",
-                    "buttons": [{"text": "Go to site","postback": website}]
+                    "buttons": [{"text": "Go to site","postback": r["website"]}]
                 }
             },
-            {
-                "text": {
-                    "text": [
-                        "Phone : " + phone + "\n",
-                        "Category : " + category + "\n",
-                        "Price Range : " + price_range + "\n",
-                        "Take out : " + take_out + "\n",
-                        "Delivery : " + delivery + "\n",
-                        "Reservation : " + reservation + "\n",
-                        "Opening Hours : \n" + officeHours
-                    ]
-                }
-            }
+            # {
+            #     "text": {
+            #         "text": [
+            #             # "Phone : " + phone + "\n",
+            #             "Category : " + r["category"] + "\n",
+            #             # "Price Range : " + price_range + "\n",
+            #             # "Take out : " + take_out + "\n",
+            #             # "Delivery : " + delivery + "\n",
+            #             # "Reservation : " + reservation + "\n",
+            #             # "Opening Hours : \n" + officeHours
+            #         ]
+            #     }
+            # }
         ]
+
+
+    # defaultRichMessage = [
+    #         {
+    #             "card": {
+    #                 "title": RestName, "subtitle": fullAddress,
+    #                 "imageUri": "",
+    #                 "buttons": [{"text": "Go to site","postback": website}]
+    #             }
+    #         },
+    #         {
+    #             "text": {
+    #                 "text": [
+    #                     "Phone : " + phone + "\n",
+    #                     "Category : " + category + "\n",
+    #                     "Price Range : " + price_range + "\n",
+    #                     "Take out : " + take_out + "\n",
+    #                     "Delivery : " + delivery + "\n",
+    #                     "Reservation : " + reservation + "\n",
+    #                     "Opening Hours : \n" + officeHours
+    #                 ]
+    #             }
+    #         }
+    #     ]
 
 
     RichMessages =  {
@@ -148,26 +268,72 @@ def processRestaurantInfoIntent(req):
     }
 
     # If there are too many no Info then return this messages
-    if infoCount > 8:
-        RichMessages = "There is no information for the " + Rest_Name
-        return make_response(jsonify({'fulfillmentText': RichMessages}))
+    # if infoCount > 8:
+    #     RichMessages = "There is no information for the " + RestaurantName
+    #     return make_response(jsonify({'fulfillmentText': RichMessages}))
 
     # Return the Restaurant Information
     return make_response(jsonify(RichMessages))
 
 
 
-def getRestaurantInfoIntentHandler(RestaurantName):
+def getRestaurantInfoIntentHandler(restaurantName, location=DEFAULT_LOCATION):
     """
-    Get RestaurantName parameter from dialogflow and call the util function `getWeatherInfo` to get weather info
+    Use Yelp APIs to get restaurant details
     """
-    url = "https://www.yelp.com/biz/" + RestaurantName + "?osq=Restaurants"
+    # url = "https://www.yelp.com/biz/" + restaurantName + "?osq=Restaurants"
+    # temp1 = scrape(url)
 
-    temp1 = parse(url)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-q', '--term', dest='term', default=DEFAULT_TERM,
+    #                     type=str, help='Search term (default: %(default)s)')
+    # parser.add_argument('-l', '--location', dest='location',
+    #                     default=DEFAULT_LOCATION, type=str,
+    #                     help='Search location (default: %(default)s)')
+    # input_values = parser.parse_args()
+
+    try:
+        temp1 = yelp_query_api(restaurantName, location)
+    except HTTPError as error:
+        if (DEBUG_MODE): print("HTTP ERROR! " + error.code)
+        return None
+
+    if (temp1 == None): return None
+    else:
+        query_result = {
+            "hours": "No Info",
+            "name": temp1["name"],
+            "phone": temp1["phone"],
+            "address": ", ".join(temp1["location"]["display_address"]),
+            "price_range": temp1["price"],
+            "category": ", ".join(map(lambda x: x["alias"], temp1["categories"])),
+            "website": temp1["url"]
+        }
+        # officeHours = "No Info"
+        # restName  = temp1["name"]
+        # phone = temp1["phone"]
+        # address = ", ".join(temp1["location"]["display_address"])
+        # price_range = temp1["price"]
+        # category = ", ".join(map(lambda x: x["alias"], temp1["categories"]))
+        # website = temp1["url"]
+        # reservation = "No Info"
+        # take_out = "No Info"
+        # delivery = "No Info"
+        # infoCount = 0
+
+    # return infoCount, website, restName, address, phone, category, price_range, officeHours, take_out, delivery, reservation
+    return query_result
+
+def getRestaurantInfoIntentHandlerByScraping(restaurantName, location=DEFAULT_LOCATION):
+    """
+    Use Web Scrapping to get restaurant details from Yelp
+    """
+    url = "https://www.yelp.com/biz/" + restaurantName + "?osq=Restaurants"
+    temp1 = scrape(url)
 
     returnText = ""
     officeHours = ""
-    RestName  = ""
+    restName  = ""
     phone = ""
     address = ""
     price_range = ""
@@ -190,9 +356,9 @@ def getRestaurantInfoIntentHandler(RestaurantName):
         officeHours = "No Info"
         infoCount += 1
     if temp1["name"] != "":
-        RestName = temp1['name']
+        restName = temp1['name']
     else:
-        RestName = "No Info"
+        restName = "No Info"
         infoCount += 1
     if temp1["phone"] != "":
         phone = temp1['phone']
@@ -247,4 +413,4 @@ def getRestaurantInfoIntentHandler(RestaurantName):
     with open("scraped_data-restaurant.json",'w') as fp:
         json.dump(temp1,fp,indent=4)
 
-    return infoCount, website, RestName, address, phone, category, price_range, officeHours, take_out, delivery, reservation
+    return infoCount, website, restName, address, phone, category, price_range, officeHours, take_out, delivery, reservation
